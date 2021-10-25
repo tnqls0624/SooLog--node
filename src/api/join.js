8fab0cadda532e1fcee632d73cd86eb451218c98
ィスビン <dktnqls0624@naver.com>
2021-10-25 17:43:19 +0900

    name property Update

--- src/api/join.js
+++ src/api/join.js
@@ -10,18 +10,28 @@ join.get('/join', async (ctx) => {
 join.post('/join/joinSuccess', async (ctx) => {
   const data = ctx.request.body;
   const client = await DBC();
-  const user = await client.findOne({ devices: ctx.login });
-
-  if (user.login === data.login) {
-    msg(`${user.login} 는 이미 존재하는 아이디입니다.`);
-    // (`${user.login} 는 이미 존재하는 아이디입니다.`);
-    ctx.redirect('/api/join');
+  console.log(data);
+  const user = await client.findOne({ id: data.id, pw: data.pw });
+  console.log(user);
+  if (!user) {
+    if (data.pw !== data.pw2) {
+      msg(`패스워드가 다릅니다. 패스워드를 확인해주세요`);
+      ctx.redirect('/api/join');
+    }
+    const result = {
+      id: data.id,
+      pw: data.pw,
+    };
+    console.log(result);
+    await client.insertOne(result);
+    ctx.body = 'JoinSuccess';
+  } else {
+    if (user.id === data.id) {
+      msg(`${user.id} 는 이미 존재하는 아이디입니다.`);
+      // (`${user.login} 는 이미 존재하는 아이디입니다.`);
+      ctx.redirect('/api/join');
+    }
   }
-  if (data.pw !== data.pw2) {
-    msg(`패스워드가 다릅니다. 패스워드를 확인해주세요`);
-    ctx.redirect('/api/join');
-  }
-  await client.insertOne(data);
 });
 
 module.exports = join;
--- src/api/login.js
+++ src/api/login.js
@@ -1,8 +1,23 @@
 const Router = require('koa-router');
+const DBC = require('./DBC');
 const login = new Router();
 
 login.get('/login', async (ctx) => {
   await ctx.render('login');
+});
+
+login.post('/login/loginSuccess', async (ctx) => {
+  const data = ctx.request.body;
+  const client = await DBC();
+  const user = await client.findOne({
+    id: data.id,
+    pw: data.pw,
+  });
+  if (data.id === user.id && data.pw === user.pw) {
+    ctx.body = `${user.name}님 안녕하세요!`;
+  } else {
+    ctx.body = `로그인 실패`;
+  }
 });
 
 module.exports = login;
