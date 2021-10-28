8095c78bce502704f910ea2d10666e01557ef0b3
ィスビン <dktnqls0624@naver.com>
2021-10-28 18:26:41 +0900

    Location setting and token setting, join page using regular expressions

--- src/api/join.js
+++ src/api/join.js
@@ -1,51 +1,74 @@
-var msg = require('alert');
 const Router = require('koa-router');
 const join = new Router();
 const DBC = require('./DBC');
 const bcrypt = require('bcrypt');
-const refreshToken = require('../token/refreshToken');
 
-join.post('/join', async (ctx) => {
+join.get('/join', async (ctx) => {
   await ctx.render('join');
 });
 
 join.post('/join/joinSuccess', async (ctx) => {
   //클라이언트 측에서의 데이터
   const data = ctx.request.body;
-
+  const re = /^[a-zA-Z0-9]{4,12}$/;
   // 모듈을 사용하여 데이터 베이스 연결
   const client = await DBC();
 
   //데이터 베이스에서 클라이언트의 ID에 해당하는 유저 정보를 가져온다.
   const user = await client.findOne({ id: data.id, pw: data.pw });
 
-  // 유저가 없을경우 로그인 실패 및 리다이렉트
-  if (!user) {
-    if (data.pw !== data.pw2) {
-      msg(`패스워드가 다릅니다. 패스워드를 확인해주세요`);
-      ctx.redirect('/api/join');
+  // 유저가 없을경우
+  const bool = check(re, data.id);
+  if (bool === true) {
+    if (!user) {
+      if (data.pw !== data.pw2) {
+        //입력 패스워드와 확인패스워드가 맞지 않을경우
+        ctx.res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        ctx.res.write(
+          "<script>alert('패스워드가 다릅니다. 패스워드를 확인해주세요')</script>"
+        );
+        ctx.res.write('<script>window.location="/api/join"</script>');
+      } else if (data.pw === data.pw2) {
+        const password = data.pw;
+
+        // bcrypt api를 이용한 패스워드의 암호화
+        const encodedPassword = await bcrypt.hash(password, 10);
+
+        // 암호화 한 패스워드와 클라이언트측의 정보를 모아 데이터베이스에 인설트
+        const result = {
+          id: data.id,
+          name: data.name,
+          pw: encodedPassword,
+        };
+        await client.insertOne(result);
+        // 성공시 성공 메시지를 띄움
+        ctx.res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        ctx.res.write(`<script>alert('회원가입에 성공하셨습니다!')</script>`);
+        ctx.res.write('<script>window.location="/"</script>');
+      } else {
+        throw new Error('error');
+      }
+    } else {
+      // 클라이언트가 보낸 ID와 데이터베이스의 ID가 같다면 중복 메세지를 띄우고 리다이렉트
+      if (user.id === data.id) {
+        ctx.res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        ctx.res.write(`<script>alert('해당 유저는 이미 존재합니다')</script>`);
+        ctx.res.write('<script>window.location="/api/join"</script>');
+      }
     }
-    const password = data.pw;
-
-    // bcrypt api를 이용한 패스워드의 암호화
-    const encodedPassword = await bcrypt.hash(password, 10);
-
-    // 암호화 한 패스워드와 클라이언트측의 정보를 모아 데이터베이스에 인설트
-    const result = {
-      id: data.id,
-      name: data.name,
-      pw: encodedPassword,
-    };
-    await client.insertOne(result);
-    // 성공시 성공 메시지를 띄움
-    ctx.body = 'JoinSuccess';
   } else {
-    // 클라이언트가 보낸 ID와 데이터베이스의 ID가 같다면 중복 메세지를 띄우고 리다이렉트
-    if (user.id === data.id) {
-      msg(`${user.id} 는 이미 존재하는 아이디입니다.`);
-      ctx.redirect('/api/join');
-    }
+    ctx.res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    ctx.res.write(`<script>alert('형식에 맞지않는 아이디 입니다.')</script>`);
+    ctx.res.write('<script>window.location="/api/join"</script>');
   }
 });
+
+function check(re, item) {
+  if (!re.test(item)) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 module.exports = join;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,21 +5,23 @@ const DBC = require('../api/DBC');
 
 let auth = async (ctx, next) => {
   const client = await DBC();
+  //해당 토큰을 받아온다
   let _accessToken = ctx.cookies.get('access_token');
   let _refreshToken = ctx.cookies.get('refresh_token');
+
   const user = await client.findOne({
     token: _refreshToken,
   });
   //액세스 토큰이 없는경우
   if (_accessToken === null) {
-    ctx.body = '접근권한이 없습니다.';
+    await ctx.render('notAccess');
   }
   const acToken = await decodeToken(_accessToken);
   const rfToken = await decodeToken(_refreshToken);
   if (acToken === undefined) {
     if (rfToken === undefined) {
       // 액세스 토큰도 없고 리프레시 토큰도 없는경우
-      throw new Error('접근권한이 없습니다.');
+      await ctx.render('notAccess');
     } else {
       //액세스 토큰은 없고 리프레시 토큰은 있는 경우
       const newToken = await generateToken({
@@ -37,15 +39,21 @@ let auth = async (ctx, next) => {
   } else {
     if (rfToken === undefined) {
       //액세스 토큰은 있고 리프레시 토큰이 없는경우
-      const newrefreshToken = await refreshToken();
+      //새로운 refreshToken을 생성하고 쿠키에 새로 세팅 후 DB에 투입
+      const newRefreshToken = await refreshToken();
+      ctx.cookies.set('refresh_token', newRefreshToken, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      });
       await client.updateOne(
         { id: user.id },
         {
           $set: {
-            token: newrefreshToken,
+            token: newRefreshToken,
           },
         }
       );
+      await next();
     } else {
       // 전부 다 있는경우
       await next();
