c6ddf192cdeb9bcdab0fc5c6beed6ff75a4b003c
ィスビン <dktnqls0624@naver.com>
2021-11-01 17:55:30 +0900

    Create Board

---
+++ src/public/post.js
@@ -0,0 +1,53 @@
+(() => {
+  function get2digits(num) {
+    return ('0' + num).slice(-2);
+  }
+
+  function getDate(dateObj) {
+    if (dateObj instanceof Date) {
+      return (
+        dateObj.getFullYear() +
+        '-' +
+        get2digits(dateObj.getMonth() + 1) +
+        '-' +
+        get2digits(dateObj.getDate())
+      );
+    }
+  }
+
+  function getTime(dateObj) {
+    if (dateObj instanceof Date) {
+      return (
+        get2digits(dateObj.getHours()) +
+        '-' +
+        get2digits(dateObj.getMinutes) +
+        '-' +
+        get2digits(dateObj.getSeconds())
+      );
+    }
+  }
+
+  function convertDate() {
+    const data_date = document.querySelectorAll('#data_date');
+    data_date.forEach((item) => {
+      const dateString = item.dataset['date'];
+      if (dateString) {
+        const date = new Date(dateString);
+        item.innerHTML = getDate(date);
+      }
+    });
+  }
+
+  function convertDateTime() {
+    const dataDate_time = document.querySelectorAll('#dataDate-time');
+    dataDate_time.forEach((item) => {
+      const dateString = item.dataset['date-time'];
+      if (dateString) {
+        const date = new Date(dateString);
+        item.innerHTML = getDate(date) + ' ' + getTime(date);
+      }
+    });
+  }
+  convertDate();
+  convertDateTime();
+})();
