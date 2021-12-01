d77fb015e0e03ccd1f8e9ba0cbc4aea6bb9bf89c
ィスビン <dktnqls0624@naver.com>
2021-12-01 17:49:22 +0900

    Naver IT News Crawling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./src/html/**/*.{html,js,jsx,ts,tsx}'],
+  purge: {
+    enabled: true,
+    content: ['./src/html/**/*.pug'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      width: {
-        'w-80': '22rem',
+      lineClamp: {
+        3: '3',
+        8: '8',
+        9: '9',
+        10: '10',
       },
     },
     variants: {},
-    plugins: [],
+    plugins: [require('@tailwindcss/line-clamp')],
   },
 };
